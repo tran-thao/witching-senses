207e32606b3ec8d31ca6c2838b6294a612246a90
Snehaaanand <126122643+Snehaaanand@users.noreply.github.com>
2024-05-10 11:01:22 +1000

    Component error fixed

--- Assets/Scripts/ChestScript.cs
+++ Assets/Scripts/ChestScript.cs
@@ -20,8 +20,9 @@ using UnityEngine;
         GetComponent<ParticleSystem>().Stop();  // Stop the particle system (if it's playing)
 
         levelManagerTouch = GameObject.Find("LevelManagerTouch").GetComponent<LevelManagerTouch>();
-        popUpTouch = GameObject.Find("PopUpTouch").GetComponent<PopUpTouch>();
-        
+        // popUpTouch = GameObject.Find("PopUpTouch").GetComponent<PopUpTouch>();
+        popUpTouch = GetComponent<PopUpTouch>();
+
     }
 
         void OnTriggerEnter2D(Collider2D other)
